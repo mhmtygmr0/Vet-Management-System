e6f30865c9442a6458f666163c7b183380ff43bd
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-07-23 13:13:01 +0300

    connecting the database

---
+++ src/main/java/com/vetmanagement/VetmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.vetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetmanagement/VetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
